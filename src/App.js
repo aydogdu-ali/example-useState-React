023a91ea5c8de42596394e7bae4483f5126a46fa
Ali <ali.ozgur@example.com>
2023-01-13 04:01:27 +0300

    add picture

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 
 import './App.css';
 import UseStateBasic from './UseStateBasic'; // kullanacağımız componenti impot ediyorum.
+import usestatepic from "./usestatepic.jpg"
 
 function App() {
+
+
+
+
+  
   return (
     <>
-      <div className="App">
-        UseState Hook'unun Temel Kullanımı
-
-      </div>
-      <UseStateBasic/>
+      <div className="App">UseState Hook'unun Temel Kullanımı</div>
+      <UseStateBasic />
+      <section className="App">
+        <p> Kod Detayı</p>
+        <a target="_blank" href={usestatepic}>
+          <img
+            className="pic"
+            src={usestatepic}
+            alt="picture"
+            style={{ width: "350px" }}
+          />
+        </a>
+      </section>
     </>
   );
 }
